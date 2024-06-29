b09258d4e4b2c6acf78b469b6aab591f33644a67
unknown <muhdhassan.suffah@gmail.com>
2024-06-29 01:12:06 -0700

    ok

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React  from 'react'
+import Gallery from './components/Gallery.jsx'
+import TodoList from './components/TodoList.jsx'
+import './App.css'
+
+
+
+function App(){
+  return (<div className='main'>
+  <h1>HassanAfridi</h1>
+  <h2>HassanAfridi</h2>
+  </div> )
+}
+
+
+
+
+
+const ok = () =>{
+ <div>
+  return(
+   <h1>First <Gallery></Gallery></h1>
+   <Gallery />
+    
+  )
+
+ </div>
+}
+
+
+
+export default App
---
+++ src/components/Gallery.jsx
@@ -0,0 +1,14 @@
+import Profile from "./Profile.jsx";
+
+
+export default function Gallery(){
+    return(
+     <div>
+
+
+      <h1>Amazing Scientist</h1>
+      <Profile />
+     </div>
+
+    )
+}
